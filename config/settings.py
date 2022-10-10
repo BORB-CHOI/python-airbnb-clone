338ab4543cd3b0b2eae7906c05f4949bb373005f
BORB-CHOI <qzsec299@naver.com>
2022-10-10 19:38:53 +0900

    add command & s3 setup

--- config/settings.py
+++ config/settings.py
@@ -191,6 +191,7 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = "airbnb-study-s3"
+    AWS_AUTO_CREATE_BUCKET = True
     AWS_DEFAULT_ACL = "public-read"
 
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
